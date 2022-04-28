f953e7837d7561eef00abf1d2fbca9b1591eb9ed
DlililS <ladus2004daria@gmail.com>
2022-04-29 01:39:57 +0300

    Initial commit

---
+++ 5.1.cpp
@@ -0,0 +1,98 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int* osvpam(int* arr, int size);
+
+void zap(int* arr, int size);
+
+void vivod(int* arr, int size);
+
+int* osv(int* arr);
+
+int main()
+
+{
+
+	int* arr = NULL;
+
+	char fur = 0;
+
+	do
+
+	{
+
+		int size = 0;
+
+		cout << "Enter array size: ";
+
+		cin >> size;
+
+		arr = osvpam(arr, size);
+
+		zap(arr, size);
+
+		vivod(arr, size);
+
+		arr = osv(arr);
+
+		cout << "If you want to continue, press '1', close -press '0': ";
+
+		cin >> fur;
+
+	} while (fur != '0');
+
+	return 0;
+
+}
+
+int* osvpam(int* arr, int size)
+
+{
+
+	arr = new int[size];
+
+	return arr;
+
+}
+
+void zap(int* arr, int size)
+
+{
+
+	for (int i = 0; i < size; i++)
+
+	{
+
+		arr[i] = i;
+
+	}
+
+}
+
+void vivod(int* arr, int size)
+
+{
+
+	for (int i = 0; i < size; i++)
+
+	{
+
+		cout << arr[i] << " ";
+
+	}
+
+	cout << endl;
+
+}
+
+int* osv(int* arr)
+{
+
+	delete[] arr;
+
+	arr = NULL;
+
+	return arr;
+
+}
---
+++ 5.3.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+
+#include <ctime>
+
+using namespace std;
+void znach(int** n1, int n2, int n3);
+void vivod(int** n1, int n2, int n3);
+
+int main()
+
+{
+
+	int strings = 0;
+
+	int stolbs = 0;
+
+	cout << "Enter the dimensions of the two-dimensional array" << endl;
+
+	cout << "Number of rows:";
+
+	cin >> strings;
+
+	cout << "Number of columns:";
+
+	cin >> stolbs;
+
+	int** chisla = new int* [strings];
+
+	for (int i = 0; i < strings; i++)
+
+	{
+		chisla[i] = new int[stolbs];
+	}
+
+	znach(chisla, strings, stolbs);
+
+	vivod(chisla, strings, stolbs);
+
+	for (int i = 0; i < strings; i++)
+
+	{
+		delete[] chisla[i];
+	}
+
+	delete[] chisla;
+
+	return 0;
+
+}
+
+void znach(int** n1, int n2, int n3)
+
+{
+	srand(time(0));
+
+	for (int i = 0; i < n2; i++){
+		for (int j = 0; j < n3; j++)
+		{
+			n1[i][j] = 10 + rand() % 40;
+		}
+	}
+
+}
+
+void vivod(int** n1, int n2, int n3)
+{
+
+	cout << endl;
+
+	for (int i = 0; i < n2; i++)
+	{
+		cout << " ";
+
+		for (int j = 0; j < n3; j++)
+		{
+			cout << n1[i][j] << " ";
+		}
+		cout << " " << endl;
+
+	}
+
+}
